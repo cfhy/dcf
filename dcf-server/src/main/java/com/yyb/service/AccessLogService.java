0abae424a1e52a0b7a75ea747f9304af5aada69d
yangyb <947531076@qq.com>
2021-09-25 15:28:03 +0800

    修改价格

--- dcf-server/src/main/java/com/yyb/service/AccessLogService.java
+++ dcf-server/src/main/java/com/yyb/service/AccessLogService.java
@@ -1,6 +1,7 @@
 package com.yyb.service;
 
 
+import cn.hutool.core.util.StrUtil;
 import com.yyb.entity.AccessLog;
 import com.yyb.mapper.AccessLogMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -21,8 +22,27 @@ public class AccessLogService {
         log.setAccess_time(new Date());
         log.setAccess_stock_code(stockCode);
         log.setAccess_stock_name(stockName);
-        log.setAccess_ip(request.getRemoteAddr());
+        log.setAccess_ip(getIpAddr(request));
         log.setUser_agent(request.getHeader("User-Agent"));
         accessLogMapper.insert(log);
+    }
+
+    public String getIpAddr(HttpServletRequest request) {
+        String ipAddress = request.getHeader("x-forwarded-for");
+        if (StrUtil.isBlank(ipAddress) || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+        if (StrUtil.isBlank(ipAddress) || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (StrUtil.isBlank(ipAddress) || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+        }
+        if (StrUtil.isBlank(ipAddress) || ipAddress.contains(":")
+                || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = "127.0.0.1";
+        }
+        String [] ips= ipAddress.split(",");
+        return ips[0];
     }
 }
